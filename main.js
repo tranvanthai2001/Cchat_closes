bf63d18ffd8e91ed86f3fda0af7be1b950017f4e
Trần Văn Thái <tranvanthai171101@gmail.com>
2023-08-24 01:19:53 +0700

    style: ui

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const navMenu = document.querySelector(".box_menu"),
   onMenu_mb = document.querySelector(".menu_icon"),
-    offMenu_mb = document.querySelector(".icon_close");
+  offMenu_mb = document.querySelector(".icon_close");
 
-onMenu_mb.addEventListener("click", () => {
-  navMenu.classList.add("show");
+onMenu_mb.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (!navMenu.classList.contains("show")) {
+    navMenu.classList.add("show");
+  }
 });
+
 offMenu_mb.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
+
+document.addEventListener('click', (e) => {
+  if (!navMenu.contains(e.target) && navMenu.classList.contains("show")) {
     navMenu.classList.remove("show");
-  });
+  }
+});
